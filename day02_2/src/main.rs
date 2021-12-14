bbe2891d05fa1cff891c12ad8fd206b4daf09219
Debashis Biswas <debashis.biswas@asu.edu>
2021-12-14 12:38:02 -0700

    solved day 2, second puzzle

---
+++ day02_2/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+fn main() {
+    // https://doc.rust-lang.org/book/ch12-02-reading-a-file.html
+    let contents = fs::read_to_string("input.txt").expect("Could not read file");
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for line in contents.lines() {
+        let (dir, num) = line.split_once(' ').unwrap();
+        let num: i32 = num.parse().unwrap();
+        match dir {
+            "forward" => {
+                horizontal += num;
+                depth += aim * num;
+            }
+            "down" => aim += num,
+            "up" => aim -= num,
+            _ => println!("Encountered unexpected input: {}", dir),
+        }
+    }
+    println!("horizontal * depth = {}", horizontal * depth);
+}
